09b65feb308a972f2fcd2bae23bb655f25ea985b
Gabriel Castillo <jesus.castilloc@outlook.com>
2023-01-26 11:22:13 -0300

    se inserta el video de fondo

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,11 +1,18 @@
 import { Nav } from "../../nav/Nav";
+import  Video  from "../../../assets/images/video_construccion.mp4"
 import "./Home.css";
 
 export const Home = () => {
     
     return (<>
         <Nav/>
-        <h1>EBG Ingeniería y Construcción</h1>
+        <header className="header_home">
+            <video className="fondo_header_home" loop autoPlay muted poster="https://carontestudio.com/img/contacto.jpg">
+                <source src={Video} type="video/mp4"/>
+            </video>
+            <h1>EBG Ingeniería y Construcción</h1>
+        </header>
+        
         
     </>)
 }
