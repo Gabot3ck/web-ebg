3e864ffb23f6dcf48ac5c27d190f251279a3fea2
Gabriel Castillo <jesus.castilloc@outlook.com>
2023-01-27 10:27:38 -0300

    se maqueta el componente QueHacemos

--- src/components/pages/home/que_hacemos/QueHacemos.jsx
+++ src/components/pages/home/que_hacemos/QueHacemos.jsx
@@ -1,11 +1,21 @@
 import "./QueHacemos.css";
+import imgHerramientas from "../../../../assets/images/herramientas.png"
+import { Link } from "react-router-dom";
 
 export const QueHacemos = () => {
 	return (<>
-		<section className="wrapper_queHacemos w-100 mx-auto d-flex justify-content-center">
-			<main className="main_queHacemos px-3">
-				<h2 className="text-center py-4 ">¿QUE HACEMOS?</h2>
-				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset</p>
+		<section className="wrapper_queHacemos w-100 mx-auto d-flex justify-content-center ">
+
+			<main className="main_queHacemos ps-5 ps-xxl-0">
+				<div className="main_queHacemos_texto">
+					<h2 className="text-start py-4 ">¿QUE HACEMOS?</h2>
+					<p className="text-start">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it.</p>
+					<Link className="btn btn-success mt-4">Acerca de nosotros</Link>
+				</div>
+				<div className="main_queHacemos_img  d-flex justify-content-center">
+					<img src={ imgHerramientas } alt="Herramientas usadas en la remodelaciones de EBG Chile" />
+				</div>
+				
 			</main>
 		</section>
 	</>)
