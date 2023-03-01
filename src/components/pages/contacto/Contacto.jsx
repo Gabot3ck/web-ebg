19c99dca57546bf7943da14c25b0a28cb801eaaa
Gabriel Castillo <jesus.castilloc@outlook.com>
2023-03-01 11:09:32 -0300

    se termina de hacer la página de contacto

--- src/components/pages/contacto/Contacto.jsx
+++ src/components/pages/contacto/Contacto.jsx
@@ -1,32 +1,37 @@
 import { Nav } from "../../nav/Nav";
 import { Footer } from "../footer/Footer";
 import { FormContacto } from "./FormContacto";
+import { Ubicacion } from "./Ubicacion";
+import { BtnWhatsApp } from "../../../helpers/boton_whatsapp/BtnWhatsApp";
 import "./Contacto.css";
+
 
 export const Contacto = () => {
     return (<>
-        <Nav/>
+        <div className="fondo_nav mb-4" >
+
+        </div>
         <section className="container_contacto w-100 ">
-            
-            <h1 className="mx-auto text-center" >Contáctenos</h1>
+            <Nav/>
+            <h1 className="mx-auto text-center mb-4" >Contáctenos</h1>
             <div className="wrapper_contacto d-flex gap-3 justify-content-between mx-auto py-3">
 
                 <div className="contacto_formulario w-50 d-flex flex-column">
                     <p className="" >
                     Llame o envíe nuestro formulario en línea para solicitar un presupuesto o para preguntas generales, dudas o consultas. <br /> ¡Esperamos poder servirle!
                     </p>
-
                     <FormContacto />
-                    
                 </div>
 
-                <div className="contacto_ubicacion w-50 d-flex flex-column bg-warning">
-                    <p>Ubiquenos</p>
+                <div className="contacto_ubicacion w-50 d-flex flex-column">
+                    <p className="text-center h4 mb-3">Ubíquenos</p>
+                    <Ubicacion />
                 </div>
             </div>
-
+            <Footer />
+            <BtnWhatsApp />
         </section>
-        <Footer />
+        
         
     </>)
 }
---
+++ src/components/pages/contacto/Ubicacion.jsx
@@ -0,0 +1,46 @@
+
+import "./Contacto.css"
+
+export const Ubicacion = () => {
+
+
+
+    return (<>
+        <section className="w-100 d-flex flex-column h-100 gap-5">
+
+            <div className="datos d-flex gap-4 justify-content-between">
+
+                <div className="contacto w-50 d-flex flex-column">
+                    <p className="h5 mb-3" >Información de contacto</p>
+                    <div className="correo d-flex">
+                        <i className="fas fa-envelope me-2"></i>
+                        <p className="">contacto@ebgchile.cl</p>
+                    </div>
+                    <div className="fono d-flex">
+                        <i className="fas fa-phone-alt me-2"></i>
+                        <p className="">{`+56 2 3305 4785`}</p>
+                    </div>
+                    <div className="web d-flex">
+                        <i className="fas fa-globe me-2"></i>
+                        <p className="">www.ebgchile.cl</p>
+                    </div>
+                </div>
+
+                <div className="direccion w-50">
+                    <p className="h5 mb-3" >Dirección</p>
+                    <div className="direccion d-flex">
+                        <i className="fas fa-map-marker-alt me-2"></i>
+                        <p className="">Av. Fernández Albano 856 <br/> La Cisterna, Santiago RM <br/> Chile.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="mapa w-100 wrapper_mapa">
+                <iframe
+                className="w-100 h-100"
+                title="Ubicación de EBG Ingeniería y Construcción"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3326.0804124677616!2d-70.65204748492219!3d-33.52529488075252!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662daf3ea337c7d%3A0x3a07585764cdc7e!2sAv.%20Fernandez%20Albano%20856%2C%20La%20Cisterna%2C%20Regi%C3%B3n%20Metropolitana!5e0!3m2!1ses-419!2scl!4v1635643902533!5m2!1ses-419!2scl" allowFullScreen="" loading="lazy" />
+            </div>
+        </section>
+    </>)
+}
