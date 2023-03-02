cad7f3f80b16ded7d511ebda56fd0bdaa1a1bd8b
Gabriel Castillo <jesus.castilloc@outlook.com>
2023-03-02 17:47:56 -0300

    se agregan estilos al grid card con hover

--- src/helpers/grid_cards/GridCards.jsx
+++ src/helpers/grid_cards/GridCards.jsx
@@ -7,14 +7,16 @@ export const GridCards = ( { img1, alt1, img2, alt2, img3, alt3, row } ) => {
 
     return (<>
         <div className={`grid_cards_container w-50 h-100 gap-3  d-flex  p-2  ${row}`}>
-            <div className="w-50 h-100" >
+            <div className="w-50 h-100 wrapper_img " >
+                <p className=" w-100 text-center m-0" > {alt1} </p>
                 <img className="w-100 h-100 " src={ img1 } alt={ alt1 } />
+                
             </div>
             <div className="w-50 h-100 gap-3 d-flex flex-column" >
-                <div  className="w-100 h-50 bg-success " >
+                <div  className="w-100 h-50  wrapper_img " >
                     <img className="w-100 h-100 " src={ img2 } alt={ alt2 } />
                 </div>
-                <div className="w-100 h-50 bg-danger">
+                <div className="w-100 h-50 wrapper_img ">
                     <img className="w-100 h-100 " src={ img3 } alt={ alt3 } />
                 </div>
             </div>
