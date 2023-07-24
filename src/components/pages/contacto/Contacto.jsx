cadca5b49ec916651dfa73e041b984e701f298a3
Gabriel Castillo <jesus.castilloc@outlook.com>
2023-07-24 16:24:05 -0400

    responsive contacto

--- src/components/pages/contacto/Contacto.jsx
+++ src/components/pages/contacto/Contacto.jsx
@@ -8,16 +8,16 @@ export const Contacto = () => {
   return (<>
     <h2 className="mx-auto text-center mb-4 title_contacto" >CONTÁCTENOS</h2>
 
-    <div className="wrapper_contacto d-flex gap-3 justify-content-between mx-auto py-3">
+    <div className="mx-auto py-3 wrapper_contacto d-flex flex-column flex-lg-row gap-3 justify-content-between align-items-center ">
 
-      <div className="contacto_formulario w-50 d-flex flex-column">
+      <div className="contacto_formulario d-flex flex-column justify-content-center ">
         <p className="" >
         Llame o envíe nuestro formulario en línea para solicitar un presupuesto o para preguntas generales, dudas o consultas. <br /> ¡Esperamos poder servirle!
         </p>
         <FormContacto />
       </div>
 
-      <div className="contacto_ubicacion w-50 d-flex flex-column">
+      <div className="contacto_ubicacion d-flex flex-column">
         <p className="text-center h4 mb-3">Ubíquenos</p>
         <Ubicacion />
       </div>
