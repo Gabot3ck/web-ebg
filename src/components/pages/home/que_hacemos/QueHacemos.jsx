3955666d3c9d51d2dd0462db3550a04ec0df63f8
Gabriel Castillo <jesus.castilloc@outlook.com>
2023-07-27 17:33:45 -0400

    se agregan efectos al header y nosotros

--- src/components/pages/home/que_hacemos/QueHacemos.jsx
+++ src/components/pages/home/que_hacemos/QueHacemos.jsx
@@ -1,23 +1,45 @@
-import imagen from "../../../../assets/images/serviciosEBG.jpg"
+import { useInView } from 'react-intersection-observer';
+import imagen from "../../../../assets/images/serviciosEBG.jpg";
 import "./QueHacemos.css";
 
 export const QueHacemos = () => {
-	return (<>
-		<section className="wrapper_queHacemos w-100 mx-auto d-flex justify-content-center ">
 
-			<main className="main_queHacemos mx-auto  d-flex flex-column ">
+	const [ref, inView] = useInView({
+    triggerOnce: true, // Para que la animación se ejecute solo una vez
+    threshold: 0.1, // Porcentaje de intersección necesario para activar la animación (0.5 es 50%)
+  });
+
+
+	return (<>
+		<section
+			className={`wrapper_queHacemos w-100 mx-auto d-flex justify-content-center`}
+			id='nosotros'
+		>
+
+			<main 
+				ref={ref}
+				className={`main_queHacemos mx-auto  d-flex flex-column 
+				${inView ? 'animate__animated animate__fadeInUp animate__delay-1s' : ''} `}
+			>
 				<h2 className="text-center text-lg-start py-4 px-4 px-xxl-0 step-7">¿QUE HACEMOS?</h2>
 
-				<section className="main_queHacemos_texto px-4 px-xxl-0 d-flex flex-column flex-lg-row align-items-center justify-content-lg-between">
+				<section 
+					className="main_queHacemos_texto px-4 px-xxl-0 d-flex flex-column flex-lg-row align-items-center justify-content-lg-between"
+				>
 
-					<div className="order-1 order-lg-0" >
+					<div 
+						className={ `order-1 order-lg-0` } 
+					>
 						<p className="step-3">En <strong>EBG Chile</strong>, como expertos en <strong>Ingeniería y Construcción</strong> nos dedicamos profesionalmente a transformar tus ideas en realidades tangibles, ofrecemos una amplia gama de servicios para cubrir todas tus necesidades en el campo de la edificación.</p><br />
 						<p className="step-3">Con nuestras habilidades en <em>obras civiles</em>, <em>construcción</em>, remodelación y asesoría, estamos comprometidos en superar tus expectativas y entregar resultados excepcionales. </p> <br />
 						<p className="step-3"> Contáctanos hoy mismo y déjanos ser tu socio confiable.</p>
 					</div>
 
 					<div className="main_queHacemos_img  d-flex justify-content-center align-items-center order-0 order-lg-1 mb-4 mb-lg-0">
-						<img src={ imagen } alt="Herramientas usadas en la remodelaciones de EBG Chile"  />
+						<img
+							src={ imagen } 
+							alt="Obras civiles realizadas por EBG Ingeniería y Construcción"
+						/>
 					</div>
 				</section>
 				
